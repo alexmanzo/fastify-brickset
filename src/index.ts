70c6bc257796305a0c5694328a5f3148a7b57a09
Alex Manzo <amanzo@fundrise.com>
2024-01-03 18:21:13 -0500

    Add nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
+import fastifySoapClient from '@fastify/soap-client';
 
 const fastify = Fastify({
-  logger: true
+  logger: true,
 });
 const port = Number(process.env.PORT) || 3000;
 
@@ -10,12 +11,15 @@ const port = Number(process.env.PORT) || 3000;
 // decorators
 // hooks
 // my services
+fastify.get('/', function (request, reply) {
+  reply.send('cool!')
+});
 
-fastify.listen({ port },(err, address) => {
+fastify.listen({ port }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
- 
+
   fastify.log.info(`Fastify is listening on port: ${address}`);
 });
